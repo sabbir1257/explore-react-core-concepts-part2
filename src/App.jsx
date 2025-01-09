0caaf0a28ac4d9870ab7bdc33173b45affd35091
sabbir1257 <masabbir1257@gmail.com>
2025-01-09 23:55:04 +0600

    Understand the concept of useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
 import './App.css'
 import Counter from './Counter'
+import Team from './Team'
+import Users from './Users'
 
 function App() {
- function handleClick(){
-  alert('button click')
- }
- const handleClick2 = () => {
-  alert('button 2 click')
- } 
+  function handleClick() {
+    alert('button click')
+  }
+  const handleClick2 = () => {
+    alert('button 2 click')
+  }
 
- const addToFive = (num) => {
-  alert(num + 5);
- } 
+  const addToFive = (num) => {
+    alert(num + 5);
+  }
 
   return (
     <>
       <h3>React Core Concepts 2</h3>
-      
+
+      <Users></Users>
+
+      <Team></Team>
+
       <Counter></Counter>
 
       <button onClick={handleClick}>Click Me</button>
       <br /> <br />
       <button onClick={handleClick2}>Click2</button>
       <br /> <br />
-      <button onClick={() => {alert('third click')}}>Third</button>
+      <button onClick={() => { alert('third click') }}>Third</button>
       <br /><br />
       {/*  */}
       <button onClick={() => addToFive(3)}>Four</button>
